65dc0f1fb834127ab242dd4cedb814db55635122
Ismaila Diallo <50891200+ismaila10@users.noreply.github.com>
2021-01-08 11:32:25 +0100

    Factoriser Code

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
 
 const Counter = (props) => {
-  const tableau = useState(1);
+  const [compteur, setCompteur] = useState(1);
 
-  const compteur = tableau[0];
-  const setCompteur = tableau[1];
+  const handleChange = () => {
+    setCompteur(compteur + 1);
+  };
 
   return (
     <div>
       {compteur}
-      <button onClick={() => setCompteur(compteur + 1)}>Incrément</button>
+      <button onClick={handleChange}>Incrément</button>
     </div>
   );
 };
